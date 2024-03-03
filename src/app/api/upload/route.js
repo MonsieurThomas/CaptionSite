8ff02b7d05d871c8f986935551b3b4d25118e7e3
Thomas Romain <r.thomas06200@gmail.con>
2024-03-03 21:32:31 +0100

    fix:api aws

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,40 +1,33 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import uniqid from "uniqid"
+import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import uniqid from 'uniqid';
 
 export async function POST(req) {
-    // console.log("arrivé")
-    const formData = await req.formData()
-    const file = formData.get('file');
-    const {name, type} = file;
-    const data = await file.arrayBuffer();
+  const formData = await req.formData();
+  const file = formData.get('file');
+  const {name, type} = file;
+  const data = await file.arrayBuffer();
 
+  const s3client = new S3Client({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
-        // console.log("AWS_ACCES_KEY = ", process.env.AWS_ACCES_KEY)
-        // console.log("AWS_SECRET_ACCES_KEY = ", process.env.AWS_SECRET_ACCES_KEY)
-        // console.log("AWS_BUCKET_NAME =", process.env.AWS_BUCKET_NAME)
+  const id = uniqid();
+  const ext = name.split('.').slice(-1)[0];
+  const newName = id + '.' + ext;
 
-        const s3client = new S3Client({
-            region: "eu-west-3",
-            credentials:{
-                accessKeyId: process.env.AWS_ACCES_KEY,
-                secretAccessKey: process.env.AWS_SECRET_ACCES_KEY,
-            },
-        });
+  const uploadCommand = new PutObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Body: data,
+    ACL: 'public-read',
+    ContentType: type,
+    Key: newName,
+  });
 
-    const id = uniqid();
-    const ext = name.split('.').slice(-1)[0];
-    const newName = id + '.' + ext;
+  await s3client.send(uploadCommand);
 
-    const uploadCommand = new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Body: data,
-        ACL: 'public-read',
-        ContentType: type,
-        Key: newName,
-    });
-
-       await s3client.send(uploadCommand)
-        
-    return Response.json({name,ext,newName,id});
-
+  return Response.json({name,ext,newName,id});
 }
